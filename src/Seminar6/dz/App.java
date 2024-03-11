f76733d379dfa4e0966faa2841d8a27194e74700
VladimirE <wortex2014@bk.ru>
2024-03-11 17:40:56 +0300

    final

---
+++ src/Seminar6/dz/App.java
@@ -0,0 +1,14 @@
+package Seminar6.dz;
+
+/**
+ * Lesson 6
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MontyHallParadox montyHallParadox = new MontyHallParadox();
+        montyHallParadox.viewWins();
+    }
+}
